3296d1bd4ab1bafa44968fcad289a12f2c05cc24
skandikonda123 <113061383+skandikonda123@users.noreply.github.com>
2023-02-08 23:36:25 -0600

    Create datatypes.py

---
+++ _posts/python_codes/datatypes.py
@@ -0,0 +1,38 @@
+# Integer
+age = 30
+
+# Float
+price = 10.5
+
+# String
+name = "John Doe"
+
+# List
+numbers = [1, 2, 3, 4, 5]
+
+# Tuple
+person = (name, age)
+
+# Dictionary
+employee = {"name": name, "age": age}
+
+# Set
+unique_numbers = {1, 2, 3, 4, 5}
+
+# Boolean
+is_employed = True
+
+# None
+nothing = None
+
+print("Age: ", age)
+print("Price: ", price)
+print("Name: ", name)
+print("Numbers: ", numbers)
+print("Person: ", person)
+print("Employee: ", employee)
+print("Unique Numbers: ", unique_numbers)
+print("Is Employed: ", is_employed)
+print("Nothing: ", nothing)
+
+
