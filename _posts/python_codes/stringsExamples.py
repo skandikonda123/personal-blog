59bff8416fa974c7a2a95b9ed5569498e2c88121
skandikonda123 <113061383+skandikonda123@users.noreply.github.com>
2023-02-08 23:23:49 -0600

    Create stringsExamples.py

---
+++ _posts/python_codes/stringsExamples.py
@@ -0,0 +1,25 @@
+# String Declaration
+s = "Hello, World!"
+
+# Concatenation
+s1 = "Hello"
+s2 = "World"
+s3 = s1 + ", " + s2 + "!"
+
+# Slicing
+print(s[7:12])
+
+# Replacing
+s = s.replace("World", "Python")
+
+# Formatting
+name = "John"
+age = 30
+s = "My name is {} and I am {} years old".format(name, age)
+
+# Length
+print(len(s))
+
+# Case conversion
+print(s.upper())
+print(s.lower())
