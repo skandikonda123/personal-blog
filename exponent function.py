f628aba2b00c91081feae4dc9f2880ddc55c5134
skandikonda123 <113061383+skandikonda123@users.noreply.github.com>
2023-03-16 18:22:24 -0500

    Create exponent function.py

---
+++ exponent function.py
@@ -0,0 +1,27 @@
+# Example 1: Basic Exponentiation
+base = 2
+exponent = 3
+
+result = base ** exponent
+print(result) # Output: 8
+
+# Example 2: Exponentiation with Variables
+x = 5
+y = 2
+
+result = x ** y
+print(result) # Output: 25
+
+# Example 3: Exponentiation with Negative Numbers
+base = -4
+exponent = 3
+
+result = base ** exponent
+print(result) # Output: -64
+
+# Example 4: Exponentiation with Decimal Numbers
+base = 2.5
+exponent = 2
+
+result = base ** exponent
+print(result) # Output: 6.25
